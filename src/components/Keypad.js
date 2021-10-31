bb06a8b8bda50ba551b9ecef61c012260ef4f6a0
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-31 12:08:21 -0400

    commit tree 387de53df349685f95902c2689b385dcb81f4f6e

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export const Keypad = () => {
+    return (
+        <div>
+            <input onChange={(e) => {
+                console.log("Entering password...")
+        }} 
+        type="password"/> 
+        </div>
+    )
+}
+
+export default Keypad;
